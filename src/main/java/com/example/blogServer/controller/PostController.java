be4ec9082c1cb66b9c88b71a1050f17fb80a0f59
joneilson pereira <joneilson84@gmail.com>
2024-03-14 01:04:39 -0300

    fix: corrigindo CrossOrigin

--- src/main/java/com/example/blogServer/controller/PostController.java
+++ src/main/java/com/example/blogServer/controller/PostController.java
@@ -5,10 +5,7 @@ import com.example.blogServer.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/posts")
@@ -17,6 +14,7 @@ public class PostController {
     @Autowired
     private PostService postService;
 
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
     @PostMapping
     public ResponseEntity<?> createPost(@RequestBody Post post) {
         try {
