95a09cb60ff698f2196a8aa2e40da907c871fbc5
joneilson pereira <joneilson84@gmail.com>
2024-03-15 14:57:15 -0300

    feat: adicionando url para busca de comentários

--- src/main/java/com/example/blogServer/service/CommentService.java
+++ src/main/java/com/example/blogServer/service/CommentService.java
@@ -2,7 +2,12 @@ package com.example.blogServer.service;
 
 import com.example.blogServer.entity.Comment;
 
+import java.util.List;
+
 public interface CommentService {
 
-    public Comment createComment(Long postId, String postedBy, String content);
+    Comment createComment(Long postId, String postedBy, String content);
+
+    List<Comment> getCommentsByPostId(Long postId);
+
 }
